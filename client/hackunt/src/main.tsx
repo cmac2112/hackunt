0612e021ba161a5077e0520c53399869000ee8c8
Caden McArthur <cadenamcarthur@bethelks.edu>
2024-10-26 15:12:13 -0500

    making my own branch

--- client/hackunt/src/main.tsx
+++ client/hackunt/src/main.tsx
@@ -4,11 +4,18 @@ import App from './App.tsx'
 import { Auth0Provider } from '@auth0/auth0-react'
 
 const DOMAIN = import.meta.env.DOMAIN
+const CLIENTID = import.meta.env.CLIENTID
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Auth0Provider
-    domain={DOMAIN}
-    <App />
-  </StrictMode>,
-)
+      domain={DOMAIN}
+      clientId={CLIENTID}
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </StrictMode>
+);
